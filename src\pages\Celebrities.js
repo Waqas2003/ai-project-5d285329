441e44e4293e61696acc631e50b82da5277b5cc2
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:37:00 +0500

    Add src\pages\Celebrities.js

---
+++ src\pages\Celebrities.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Celebrities() {
+  return (
+    <div className="container">
+      <h1>Celebrities</h1>
+      <p>This is the celebrities page.</p>
+    </div>
+  );
+}
+
+export default Celebrities;
