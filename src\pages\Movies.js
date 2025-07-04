a03064a3bc6d363c31493e375020be10935cbc4c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:37:04 +0500

    Add src\pages\Movies.js

---
+++ src\pages\Movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <div className="container">
+      <h1>Movies</h1>
+      <p>This is the movies page.</p>
+    </div>
+  );
+}
+
+export default Movies;
