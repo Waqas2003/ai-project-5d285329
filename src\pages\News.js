67b49a289a5ed23c62d33bb63bf78b17b9130025
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:37:05 +0500

    Add src\pages\News.js

---
+++ src\pages\News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function News() {
+  return (
+    <div className="container">
+      <h1>News</h1>
+      <p>This is the news page.</p>
+    </div>
+  );
+}
+
+export default News;
